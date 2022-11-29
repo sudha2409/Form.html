0ee1a6bdffacb1d1b5c59269acdde8ab20dbe622
Abhishek Bhatia <abhishek@Abhisheks-MacBook-Air.local>
2022-11-30 01:22:27 +0530

    dashboard-prototype part1

---
+++ main.js
@@ -0,0 +1,32 @@
+async function LoadIntoTable (url , table)
+{
+    const tableHead = table.querySelector("thead");
+    const tableBody = table.querySelector("tbody");
+    const response = await fetch(url);   
+    const {headers, rows} = await response.json();
+
+    tableHead.innerHTML = "<tr></tr>";
+    tableBody.innerHTML = "";
+
+    for (const headerText of headers)
+    {
+        const headerElement = document.createElement("th");
+        headerElement.textContent = headerText;
+        tableHead.querySelector("tr").appendChild(headerElement);
+    }
+    for (const row of rows)
+    {
+        const rowElement = document.createElement("tr");
+
+        for(cellText of row)
+        {
+            const cellElement = document.createElement("td");
+            cellElement.textContent = cellText;
+            rowElement.appendChild(cellElement);
+
+        }
+
+        tableBody.appendChild(rowElement);
+    }
+}
+LoadIntoTable("./data.json",document.querySelector("table"));
